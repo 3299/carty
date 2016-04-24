c9a2db8dc30389daa5059b4c911c391fd373516d
Max Isom <codetheweb@icloud.com>
2016-04-24 15:36:32 -0500

    In theory it should now mostly work
    In practice, of course it probably won't

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # carty
 Wheelchair-like robot Just For Fun™
 
+Controlled by a joystick connected directly to an Arduino over a USB shield.
+Replaces the $1000+ FRC control system with a $30 Arduino + $18 shield.
+
 ![Wheelchair](https://s-media-cache-ak0.pinimg.com/236x/2b/63/67/2b6367100e1d385575f76e9b5e3bd6a9.jpg)
--- carty.ino
+++ carty.ino
@@ -1,26 +1,56 @@
-const int jagsigpin = 12; //pwm connection on pin 9
-const int jagpwrpin = 11;//using pin 7 as the pwm power reference
-const int jaggndpin = 10;//using pin 8 as the pwm ground reference
+const int sig[] = {13, 10, 7, 4}; // White
+const int power[] = {12, 9, 6, 3}; // Red
+const int ground[] = {11, 8, 5, 2}; // Black
 
-const int sig[] = {13, 10, 7, 4};
-const int power[] = {12, 9, 6, 3};
-const int ground[] = {11, 8, 5, 2};
+// Mins and maxes
+const int minJaguar = 20;
+const int maxJaguar = 74;
+const int neturalJaguar = 47;
+
+const int minJoystick = 0;
+const int maxJoystick = 255;
+
+// Misc
+int output[] = {0, 0, 0, 0};
+int joyX = 0;
+int joyY = 0;
 
 int i;
 
 void setup() {
+  // for loop is more efficent than setting up each pin
   for (i = 0; i < 4; i = i + 1) {
+    //Setup pins
     pinMode(sig[i], OUTPUT);
     pinMode(power[i], OUTPUT);
     pinMode(ground[i], OUTPUT);
-  }
 
-  for (i = 0; i < 4; i = i + 1) {
+    // Output power for Jaguars
     digitalWrite(power[i], HIGH);
     digitalWrite(ground[i], LOW);
+
+    // Make Jaguars start in 'friendly' state
+    analogWrite(sig[i], neturalJaguar);
   }
 }
 
 void loop() {
+  // Get and map joystick values
+  //joyx = joystick.getX()
+  //joyy = joystick.getY()
 
+  joyX = map(joyX, minJoystick, maxJoystick, minJaguar, maxJaguar);
+  joyY = map(joyY, minJoystick, maxJoystick, minJaguar, maxJaguar);
+
+
+  // Calculate outputs
+  output[0] = joyY - joyX;
+  output[1] = - joyY - joyX;
+  output[2] = - joyY - joyX;
+  output[3] = joyY - joyX;
+
+  // Run motors
+  for (i = 0; i < 4; i = i + 1) {
+    analogWrite(sig[i], output[i]);
+  }
 }
