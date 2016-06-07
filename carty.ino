3dabce878f575360ce3159dbe95cfc98e7c579b0
Max Isom <codetheweb@icloud.com>
2016-06-07 09:58:07 -0500

    Add support for joystick

--- carty.ino
+++ carty.ino
@@ -1,56 +1,100 @@
-const int sig[] = {13, 10, 7, 4}; // White
-const int power[] = {12, 9, 6, 3}; // Red
-const int ground[] = {11, 8, 5, 2}; // Black
+#include <Servo.h>
+
+const int sig[] = {11, 6, 5, 3}; // (look at top shield)
+const int power[] = {4, 2};
 
 // Mins and maxes
-const int minJaguar = 20;
-const int maxJaguar = 74;
-const int neturalJaguar = 47;
-
-const int minJoystick = 0;
-const int maxJoystick = 255;
+int minJaguar = 670;
+int maxJaguar = 2330;
 
 // Misc
-int output[] = {0, 0, 0, 0};
-int joyX = 0;
-int joyY = 0;
+int output[] = {1500, 1500, 1500, 1500};
+
+int joyX, joyY;
 
 int i;
 
+// Motors
+Servo flM, frM, blM, brM;
+
+
+//
+// Stuff for joystick
+//
+#include <usbhid.h>
+#include <hiduniversal.h>
+#include <usbhub.h>
+
+#include "le3dp_rptparser.h"
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#include <SPI.h>
+#endif
+
+USB                                             Usb;
+USBHub                                          Hub(&Usb);
+HIDUniversal                                    Hid(&Usb);
+JoystickEvents                                  JoyEvents;
+JoystickReportParser                            Joy(&JoyEvents);
+
 void setup() {
+  TCCR1B = TCCR1B & 0b11111000 | 0x04; // PWM Freq. at 122Hz
+
   // for loop is more efficent than setting up each pin
   for (i = 0; i < 4; i = i + 1) {
     //Setup pins
     pinMode(sig[i], OUTPUT);
-    pinMode(power[i], OUTPUT);
-    pinMode(ground[i], OUTPUT);
 
-    // Output power for Jaguars
-    digitalWrite(power[i], HIGH);
-    digitalWrite(ground[i], LOW);
-
-    // Make Jaguars start in 'friendly' state
-    analogWrite(sig[i], neturalJaguar);
+    // +5v is not absolutely essential to control the Jaguars but is included here for completeness
+    if (i < 3) {
+      pinMode(power[i], OUTPUT);
+      digitalWrite(power[i], HIGH);
+    }
   }
+
+  flM.attach(sig[0], minJaguar, maxJaguar);
+  frM.attach(sig[1], minJaguar, maxJaguar);
+  blM.attach(sig[2], minJaguar, maxJaguar);
+  brM.attach(sig[3], minJaguar, maxJaguar);
+
+  Serial.begin(9600);
+
+  // Joystick
+  Serial.println("Start");
+
+  if (Usb.Init() == -1)
+    Serial.println("OSC did not start.");
+
+  //delay(200);
+
+  if (!Hid.SetReportParser(0, &Joy))
+    ErrorMessage<uint8_t>(PSTR("SetReportParser"), 1  );
 }
 
 void loop() {
-  // Get and map joystick values
-  //joyx = joystick.getX()
-  //joyy = joystick.getY()
+  // Get joystick values
+  Usb.Task();
+  joyX = (int) JoyEvents.X;
+  joyY = (int) JoyEvents.Y;
 
-  joyX = map(joyX, minJoystick, maxJoystick, minJaguar, maxJaguar);
-  joyY = map(joyY, minJoystick, maxJoystick, minJaguar, maxJaguar);
-
+  joyY = map(joyY, 255, 192, 0, 255);
 
   // Calculate outputs
-  output[0] = joyY - joyX;
-  output[1] = - joyY - joyX;
-  output[2] = - joyY - joyX;
+  output[0] = joyY - joyX; //max: 255, min: -255
+  output[1] = 0 - joyY - joyX; // max: 0, min: -510
+  output[2] = 0 - joyY - joyX;
   output[3] = joyY - joyX;
 
+  output[0] = map(output[0], -255, 255, minJaguar, maxJaguar);
+  output[1] = map(output[1], -510, 0, minJaguar, maxJaguar);
+  output[2] = map(output[2], -510, 0, minJaguar, maxJaguar);
+  output[3] = map(output[3], -255, 255, minJaguar, maxJaguar);
+
   // Run motors
-  for (i = 0; i < 4; i = i + 1) {
-    analogWrite(sig[i], output[i]);
-  }
+  flM.writeMicroseconds(output[0]);
+  frM.writeMicroseconds(output[1]);
+  blM.writeMicroseconds(output[2]);
+  brM.writeMicroseconds(output[3]);
 }
