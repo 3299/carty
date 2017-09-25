53c9c1f4fcf0d4b6fa3becf120dac5cbcdcd8ba6
Max Isom <codetheweb@icloud.com>
2017-09-25 12:14:50 -0500

    Switched to finger driving

--- app/main.js
+++ app/main.js
@@ -7,6 +7,12 @@ function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
+function remap(n,i,o,r,t){return i>o?i>n?(n-i)*(t-r)/(o-i)+r:r:o>i?o>n?(n-i)*(t-r)/(o-i)+r:t:void 0}
+
+function radiansToDegrees(angle) {
+  return angle * (180/Math.PI);
+}
+
 $(document).ready(function() {
   // WebSocket
   var serverSocket = new WebSocket('ws://3299vision.local:8000/');
@@ -14,36 +20,103 @@ $(document).ready(function() {
     serverSocket.send('connected');
   }
 
+  function moveBot(tap) {
+    console.log(tap['rotation']);
+    // Compute values for wheels
+    var wheels = [];
 
-  // Add event manager for gyro
-  window.addEventListener('deviceorientation', function(e) {
-    // Alpha -> Rotation
-    // Beta  -> x-direction
-    // Gamma -> y-direction
-    var rotation = round(e.alpha, 2);
-    var xDirection = round(e.beta, 2);
-    var yDirection = round(e.gamma, 2);
+    wheels[0] = round(tap['x'] + tap['y'] + tap['rotation'], 3);  // front left
+    wheels[1] = round(-tap['x'] + tap['y'] - tap['rotation'], 3); // front right
+    wheels[2] = round(-tap['x'] + tap['y'] + tap['rotation'], 3); // back left
+    wheels[3] = round(tap['x'] + tap['y'] - tap['rotation'], 3);  // back right
 
-    // Alpha limited to 180 degrees
-    if (between(rotation, 0, 90)) { rotation = -rotation; }
-    else if (between(rotation, 270, 360)) { rotation = 360 - rotation; }
-    else if (between(rotation, 90, 180)) { rotation = -90; }
-    else if (between(rotation, 180, 270)) { rotation = 90; }
+    // Normalize values
+    var maxMagnitude = 0;
+    for (var i = 0; i <= 3; i++) {
+      if (Math.abs(wheels[i]) > maxMagnitude) {
+        maxMagnitude = Math.abs(wheels[i]);
+      }
+    }
 
-    // Beta limited to 180 degrees
-    if (between(xDirection, -180, -90)) { xDirection = -90}
-    if (between(xDirection, 90, 180)) { xDirection = 90}
+    if (maxMagnitude > 1) {
+      for (var i = 0; i <= 3; i++) {
+        wheels[i] = wheels[i] / maxMagnitude;
+      }
+    }
+    console.log(wheels)
+    //serverSocket.send(wheels['frontLeft']);
+  }
 
-    // Gamma limited to ± 25 degrees (so screen doesn't rotate)
-    if (between(yDirection, 25, 90)) { yDirection = 25; }
-    else if (between(yDirection, -90, -25)) { yDirection = -25; }
+  $('#main').bind("touchstart touchmove", function(e) {
+    //Disable scrolling by preventing default touch behaviour
+    e.preventDefault();
+    var orig = e.originalEvent;
 
-    serverSocket.send(yDirection);
+    var direction = {};
+
+    for (var i = 0; i < orig.touches.length; i++) {
+      var element = $(orig.touches[i].target);
+
+      var topLeft = element.position();
+      var width = element.width();
+      var height = element.height();
+      var padding = 100;
+
+      var center = {x: topLeft['left'] + (width / 2), y: topLeft['top'] + (height / 2)};
+      var tap = {x: orig.touches[i].pageX, y: orig.touches[i].pageY};
+
+      /*****/
+      /* X */
+      /*****/
+      if (tap['x'] > (center['x'] + (width / 2) - padding)) {
+        tap['x'] = (center['x'] + (width / 2) - padding);
+      }
+      else if (tap['x'] < (center['x'] - (width / 2) + padding)) {
+        tap['x'] = (center['x'] - (width / 2) + padding);
+      }
+
+      /*****/
+      /* Y */
+      /*****/
+      if (tap['y'] > (center['y'] + (height / 2) - padding)) {
+        tap['y'] = (center['y'] + (height / 2) - padding);
+      }
+      else if (tap['y'] < (center['y'] - (height / 2) + padding)) {
+        tap['y'] = (center['y'] - (height / 2) + padding);
+      }
+
+      /*********/
+      /* remap */
+      /*********/
+      tap['x'] = remap(tap['x'], (center['x'] - (width / 2) + padding), (center['x'] + (width / 2) - padding), -1, 1);
+      tap['y'] = remap(tap['y'], (center['y'] - (height / 2) + padding), (center['y'] + (height / 2) - padding), 1, -1);
+
+      if (element.attr('id') == 'translation-drive') {
+        direction = {x: tap['x'], y: tap['y']};
+      }
+
+      if (element.attr('id') == 'rotation-drive') {
+        direction['rotation'] = tap['x'];
+      }
+    }
+
+    if (!direction['rotation']) {
+      direction['rotation'] = 0;
+    }
+
+    // send coordinates
+    moveBot(direction);
+  });
+
+  $('#main').bind('touchend', function(e) {
+    if (e.originalEvent.touches.length == 0) { // only if no fingers are touching
+      moveBot({x: 0, y: 0, rotation: 0}); // stop the bot
+    }
   });
 
   // Hide intro when 'done' is clicked
   $('.calibrate-button').click(function(e) {
     $('#intro').hide();
-    $('#main').show();
+    $('#main').css('display', 'flex');
   });
 });
